3db4569bf7e735d03220bbe720b5d369efb2e074
vigossalamanca64@hotmail.com <67037448+MarioSalamanca64@users.noreply.github.com>
2021-06-05 19:20:44 -0500

    Primer commit

---
+++ src/app/app.routes/app.routes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { App.RoutesRoutingModule } from './app.routes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    App.RoutesRoutingModule
+  ]
+})
+export class App.RoutesModule { }
