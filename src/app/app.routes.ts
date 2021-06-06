f6154d3517b8a0124fbab6ae0329bc6d1dc215ae
MarioSalamanca64 <mariosalamanca64@hotmail.com>
2021-06-05 22:53:07 -0500

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
- {path: '' , 
- component: PagesComponent,
- //rutas hija para que funcione el segundo router-outlet
- children: [
-    {path: 'dashboard' , component: DashboardComponent},
-    {path: 'progress' , component: ProgressComponent},
-    {path: 'graficas1' , component: Graficas1Component}, 
-    {path: '' , redirectTo: '/dashboard',pathMatch: 'full'},
- ]
-},
+//  {path: '' , 
+//  component: PagesComponent,
+//  //rutas hija para que funcione el segundo router-outlet
+//  children: [
+//     {path: 'dashboard' , component: DashboardComponent},
+//     {path: 'progress' , component: ProgressComponent},
+//     {path: 'graficas1' , component: Graficas1Component}, 
+//     {path: '' , redirectTo: '/dashboard',pathMatch: 'full'},
+//  ]
+// },
  //estas rutas son como independientes y cambiar toda la pagina completa rutas padres
  {path: 'login' , component: LoginComponent},
  {path: 'register' , component: RegisterComponent},
